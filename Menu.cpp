eea23b2744c90e2fea0696bcb9fefc739122ec7d
ythree33 <82850377+ythree33@users.noreply.github.com>
2021-08-18 11:41:01 +0800

    Create Menu.cpp

---
+++ Menu.cpp
@@ -0,0 +1,16 @@
+#include "Menu.h"
+#include <stdio.h>
+
+void ShowMenu(){
+	printf("===========================================\n") ;
+	printf("\tWelcome to the SSM_System\n") ;
+	printf("===========================================\n") ;
+	printf("\t 1 - 添加学生信息\n") ;
+	printf("\t 2 - 显示学生信息\n") ;
+	printf("\t 3 - 查询指定学生信息\n") ;
+	printf("\t 4 - 编辑指定学生信息\n") ;
+	printf("\t 5 - 删除指定学生信息\n") ;
+	printf("\t 6 - 清空系统\n") ;
+	printf("\t 0 - 退出系统\n") ;
+	printf("===========================================\n") ;
+}
